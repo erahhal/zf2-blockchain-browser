8e72035dd64df26844c7f2e25c58a0fcf782fcd6
Ellis Rahhal <ellis@guestops.com>
2013-11-15 19:09:46 +0800

    first commit

---
+++ module/Application/src/Application/Entity/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Application\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\Entity */
+class User{
+    /**
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    * @ORM\Column(type="integer")
+    */
+    protected $id;
+
+    /** @ORM\Column(type="string") */
+    protected $lastName;
+
+    /** @ORM\Column(type="string") */
+    protected $firstName;
+
+    // getters/setters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+} 
