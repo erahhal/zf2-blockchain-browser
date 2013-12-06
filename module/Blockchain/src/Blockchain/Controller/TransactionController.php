1add3f6e1aab24ee625ace1d699f7b891a6f6ac3
Ellis Rahhal <ellis@guestops.com>
2013-12-06 05:23:23 -0800

    added transaction visualization

--- module/Blockchain/src/Blockchain/Controller/TransactionController.php
+++ module/Blockchain/src/Blockchain/Controller/TransactionController.php
@@ -16,9 +16,42 @@ class TransactionController extends AbstractActionController
 
     public function indexAction()
     {/*{{{*/
+        $this
+            ->getServiceLocator()
+            ->get('viewhelpermanager')
+            ->get('HeadScript')
+            ->appendFile('/vendor/js/d3.v3.min.js')
+            ->appendFile('/vendor/js/sankey.js')
+            ;
+
         $txid = $this->params('txid');
 
         $transactionData = $this->_blockchain->getTransaction($txid);
+        $nodes = array(array('index' => 0, 'name' => $transactionData['totalIn']));
+        $links = array();
+        $index = 1;
+        foreach($transactionData['inputs'] as $input) {
+            if ($input['isCoinbase']) {
+                $nodes[] = array('index' => $index, 'name' => 'Generation'); 
+            } else {
+                $nodes[] = array('index' => $index, 'name' => $input['amount'].' ('.$input['fromAddress'].')'); 
+            }
+            $links[] = array('source' => $index, 'target' => 0, 'value' => $input['amount']);
+            $index++;
+        }
+        foreach($transactionData['outputs'] as $output) {
+            $nodes[] = array('index' => $index, 'name' => $output['amount'].' ('.$output['address'].')'); 
+            $links[] = array('source' => 0, 'target' => $index, 'value' => $output['amount']);
+            $index++;
+        }
+        if ($transactionData['fee'] > 0) {
+            $nodes[] = array('index' => $index, 'name' => $transactionData['fee'].' (fees)'); 
+            $links[] = array('source' => 0, 'target' => $index, 'value' => $transactionData['fee']);
+            $index++;
+        }
+        $transactionData['nodes'] = $nodes;
+        $transactionData['links'] = $links;
+        $transactionData['chartHeight'] = max(count($transactionData['inputs']), count($transactionData['outputs'])) * 20;
 
         $view = new ViewModel($transactionData);
 
